6b6a335e3fe4e65da09408b7acfec33f7384d3c6
Paul Martz <pmartz@pauldeb.skew-matrix.com>
2015-11-23 16:11:57 -0700

    Initial commit.

---
+++ install.sh
@@ -0,0 +1,25 @@
+#/bin/sh
+
+EXEDIR=/usr/local/bin
+MANDIR=/usr/share/man/man1
+
+if [ "$1" = "-u" ]
+then
+    # Uninstall
+
+    rm -f $EXEDIR/smsbk $EXEDIR/smsre
+    rm -f $MANDIR/smsbk.1.gz $MANDIR/smsre.1.gz
+else
+    # Install
+
+    cp ./smsbk $EXEDIR
+    cp ./smsre $EXEDIR
+    gzip -c ./smsbk.1 > $MANDIR/smsbk.1.gz
+    gzip -c ./smsre.1 > $MANDIR/smsre.1.gz
+
+    chown 0.0 $EXEDIR/smsbk $EXEDIR/smsbk $MANDIR/smsbk.1.gz $MANDIR/smsre.1.gz
+    chmod 755 $EXEDIR/smsbk $EXEDIR/smsbk
+    chmod 644 $MANDIR/smsbk.1.gz $MANDIR/smsre.1.gz
+fi
+
+exit 0
