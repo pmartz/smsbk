ff01440d625bf0d397b0d6cd41b1ddab0ca63284
Paul Martz <pmartz@pauldeb.skew-matrix.com>
2015-11-27 13:03:30 -0700

    Get configuration variables from config file.

--- install.sh
+++ install.sh
@@ -2,6 +2,7 @@
 
 EXEDIR=/usr/local/bin
 MANDIR=/usr/share/man/man1
+CONFDIR=/etc/default
 
 if [ "$1" = "-u" ]
 then
@@ -9,6 +10,7 @@ then
 
     rm -f $EXEDIR/smsbk $EXEDIR/smsre
     rm -f $MANDIR/smsbk.1.gz $MANDIR/smsre.1.gz
+    rm -f $CONFDIR/smsbk.conf
 else
     # Install
 
@@ -16,10 +18,11 @@ else
     cp ./smsre $EXEDIR
     gzip -c ./smsbk.1 > $MANDIR/smsbk.1.gz
     gzip -c ./smsre.1 > $MANDIR/smsre.1.gz
+    cp ./smsbk.conf $CONFDIR
 
-    chown 0.0 $EXEDIR/smsbk $EXEDIR/smsbk $MANDIR/smsbk.1.gz $MANDIR/smsre.1.gz
+    chown 0.0 $EXEDIR/smsbk $EXEDIR/smsbk $MANDIR/smsbk.1.gz $MANDIR/smsre.1.gz $CONFDIR/smsbk.conf
     chmod 755 $EXEDIR/smsbk $EXEDIR/smsbk
-    chmod 644 $MANDIR/smsbk.1.gz $MANDIR/smsre.1.gz
+    chmod 644 $MANDIR/smsbk.1.gz $MANDIR/smsre.1.gz $CONFDIR/smsbk.conf
 fi
 
 exit 0
